76f1e02b0cb2d646880a9806184400b76a6ca411
anderson.mann <anderson.mann@MKP008.4all.local>
2018-01-15 19:03:09 -0200

    classes e interfaces iniciais, padrao strategy

---
+++ design-patterns/src/CalculadorDeImpostos.java
@@ -0,0 +1,9 @@
+
+public class CalculadorDeImpostos {
+
+	public void realizaCalculo(Orcamento orcamento, Imposto impostoQualquer) {
+		double imposto = impostoQualquer.calcula(orcamento);
+		System.out.println(imposto);
+	}
+
+}
